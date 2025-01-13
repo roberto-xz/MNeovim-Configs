3e2f588431df99f899bd01d21922f221a8a6c095
roberto-xz <devv-robert@outlook.com>
2025-01-13 12:23:36 -0300

    Corrigido Ctrl-w, e modificado Atalho para mover linhas, de Shift-<Up/Down>, para Ctrl-<Up/Down>

--- lua/shortkmaps.lua
+++ lua/shortkmaps.lua
@@ -2,12 +2,12 @@
 local close_buffer = require('src_scripts.close_buffer')
 local options= {noremap=true, silent=true}
 
-vim.keymap.set({'n','i'}, "<C-w>", ":write<CR>",options)
-vim.keymap.set({'n','i'}, "<C-r>", vim.lsp.buf.rename,options)
+vim.keymap.set({'n','i'}, "<C-w>", "<Cmd>write!<CR>",options)
 vim.keymap.set({'n','i'}, "<C-k>", vim.lsp.buf.definition,options)
+vim.keymap.set({'n','i'}, "<C-r>", vim.lsp.buf.rename,options)
 vim.keymap.set({'n','i'}, "<C-b>", "<Cmd>b#<CR>",options)
-vim.keymap.set({'n','i','v'}, "<S-Up>", "<Cmd>m -2<CR>", options)
-vim.keymap.set({'n','i','v'}, "<S-Down>", "<Cmd>m +1<CR>",options)
+vim.keymap.set({'n','i','v'}, "<C-Up>", "<Cmd>m -2<CR>", options)
+vim.keymap.set({'n','i','v'}, "<C-Down>", "<Cmd>m +1<CR>",options)
 vim.keymap.set({'n','i','v'}, "<C-d>", "<ESC>yyp<CR>i",options)
 vim.keymap.set({'n','i','v'}, "<C-x>", "<Cmd>delete<CR>",options)
 vim.keymap.set({'n','i','v'}, "<C-z>", "<ESC>u<CR>i",options)
