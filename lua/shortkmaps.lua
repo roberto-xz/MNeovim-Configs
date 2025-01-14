31d8c27a2bce403527df45b0c37a3f65c4d8f543
roberto-xz <devv-robert@outlook.com>
2025-01-13 21:32:29 -0300

    Atualizado shortucts para utlizar os scripts de fechamento e swap de buffers

--- lua/shortkmaps.lua
+++ lua/shortkmaps.lua
@@ -1,8 +1,12 @@
 
 local close_buffer = require('src_scripts.close_buffer')
+local swap_buffer  = require('src_scripts.swap_buffer')
+
+local pre_quit = require('src_scripts.pre_quit')
 local options= {noremap=true, silent=true}
 
 vim.keymap.set({'n','i'}, "<C-w>", "<Cmd>write!<CR>",options)
+vim.keymap.set({'n','i'}, "<C-e>", "<Cmd>quit <CR>",options)
 vim.keymap.set({'n','i'}, "<C-k>", vim.lsp.buf.definition,options)
 vim.keymap.set({'n','i'}, "<C-r>", vim.lsp.buf.rename,options)
 vim.keymap.set({'n','i'}, "<C-b>", "<Cmd>b#<CR>",options)
@@ -11,8 +15,9 @@ vim.keymap.set({'n','i','v'}, "<C-Down>", "<Cmd>m +1<CR>",options)
 vim.keymap.set({'n','i','v'}, "<C-d>", "<ESC>yyp<CR>i",options)
 vim.keymap.set({'n','i','v'}, "<C-x>", "<Cmd>delete<CR>",options)
 vim.keymap.set({'n','i','v'}, "<C-z>", "<ESC>u<CR>i",options)
-vim.keymap.set({'n','i','v'}, "<C-Right>", "<Cmd>bn<CR>",options)
-vim.keymap.set({'n','i','v'}, "<C-Left>", "<Cmd>bp<CR>",options)
+vim.keymap.set({'n','i','v'}, "<C-Right>",function() swap_buffer('r') end)
+vim.keymap.set({'n','i','v'}, "<C-Left>",function() swap_buffer('l') end)
 
 vim.keymap.set({'i','n','v'},'<C-f>',function()vim.cmd("NvimTreeFocus") end)
 vim.keymap.set({'i','n','v'},'<C-q>',close_buffer)
+vim.keymap.set({'i','n','v'},'<C-p>',pre_quit)
