e14905d3ddd29bcd5122e666a383300789d63806
roberto-xz <devv-robert@outlook.com>
2025-01-13 21:29:01 -0300

    Refatorado, Adicinado salvamento e verificação de buffer. ( Muitos plugins utilizam buffer temporários, fechalos causa problemas )

--- lua/src_scripts/close_buffer.lua
+++ lua/src_scripts/close_buffer.lua
@@ -1,14 +1,17 @@
 
-local close_buffer = function() 
-    local buf = vim.api.nvim_get_current_buf()
-    local mod = vim.api.nvim_buf_get_option(buf,'modified')
-    if mod then 
-        vim.notify('Salve Antes de Fechar',vim.log.levels,WARN)
-        return
-    end
-    vim.cmd('bp')
-    vim.cmd('bdelete '..buf)
-    return
-end
+local close_buffer = function()
+    local buffer = vim.api.nvim_get_current_buf()
+    local buffes = vim.api.nvim_list_bufs()
+    local counts = 0;
 
+    for _,buff_temp in ipairs(buffes) do 
+       if vim.fn.buflisted(buff_temp) ~= 0 then counts = counts+1 end 
+    end
+
+    if counts > 1 then
+        vim.cmd('write!')
+        vim.cmd('bp')
+        vim.cmd('bdelete '..buffer)
+    end
+end
 return close_buffer
