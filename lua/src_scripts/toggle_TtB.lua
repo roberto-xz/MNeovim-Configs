e962a9f825f16251ebe0e03e3ddb071a5c8dbe13
roberto-xz <devv-robert@outlook.com>
2025-01-15 21:46:38 -0300

    Corrigido verificação de buffer temporário no lugar certo.

--- lua/src_scripts/toggle_TtB.lua
+++ lua/src_scripts/toggle_TtB.lua
@@ -1,19 +1,27 @@
 
-
 local toggleTtB = function ()
    local actual_buf = vim.api.nvim_get_current_buf()
    local actual_buf_name = string.sub(vim.fn.bufname(),1,8)
-
+   
+   -- focado em um buffer
    if actual_buf_name ~= 'NvimTree' then
-       _G.buffer_origen_2712a21 = actual_buf
-       vim.cmd('NvimTreeFocus')
+       -- verifica se o buffer é temporário
+       if vim.fn.buflisted(actual_buf) ~= 0 then 
+           _G.buffer_origen_2712a21 = actual_buf
+           vim.cmd('NvimTreeFocus')
+           return
+       end
+       -- o buffer é temporário
+       _G.buffer_origen_2712a21 = nil
        return
    end
-   if _G.buffer_origen_2712a21 and vim.fn.buflisted(actual_buf_name) then
+
+   -- focado no nvim-tree
+   -- verfica se o buffer origem não é temporário
+   if _G.buffer_origen_2712a21 ~= nil then
        vim.cmd('buffer '.._G.buffer_origen_2712a21)
        vim.cmd('startinsert')
    end
-   return
 end
 
 return toggleTtB
